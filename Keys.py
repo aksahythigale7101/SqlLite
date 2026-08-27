f04bf432f0bd14db3a6c23c6cf9d0c33a94064b3
aksahythigale7101 <akshaythigale7101@gmail.com>
2026-08-27 15:02:01 +0530

    Keys script add and all insert and read code making

---
+++ Keys.py
@@ -0,0 +1,67 @@
+from calendar import c
+import sqlite3
+from token import COMMA
+from Connection import _Db1 as compnayDB
+
+from tabulate import tabulate
+
+cursor = compnayDB.conn.cursor()
+
+
+class keys:
+    def InsertDepat(_iquery):
+        try:
+            Eid = _iquery[0]
+            DeprtName = _iquery[1]
+
+            # cursor=compnayDB.conn.cursor()
+            cursor.execute(
+                "INSERT INTO DEPARTMENT(DepartmentId,DepartmentName) VALUES (?,?) ",
+                (Eid, DeprtName),
+            )
+            compnayDB.conn.commit()
+
+            return True
+        except Exception as e:
+            compnayDB.conn.rollback()
+            print(f"-- {e}")
+            return False
+
+    def ReadData(TableName):
+        
+        cursor.execute(f"SELECT * FROM {TableName}")
+        compnayDB.conn.commit()
+        rows = cursor.fetchall()
+        # for row in rows:
+        #   print(row ,"--")
+        Headers = ["DepartmentId ", "DepartmentName"]
+        Headers1 = ["EmployeeId ", "Name", "DepartmentId"]
+
+      
+        
+        print(f"--{TableName}--")
+        print(
+            "\n"
+            + tabulate(
+                rows,
+                headers=Headers if TableName == "DEPARTMENT" else Headers1,
+                tablefmt="grid",
+            )
+        )
+
+    def Drop_Table():
+        cursor.execute("DROP TABLE IF EXISTS Employee")
+        compnayDB.conn.commit()
+        print("Employee table dropped successfully!")
+
+    def InsertEmp(EmpId, EName, DeptID):
+        try:
+
+            cursor.execute(
+                "INSERT INTO EMPLOYEE(EmployeeId,Name,DepartmentId) VALUES (?,?,?)",
+                (EmpId, EName, DeptID),
+            )
+            compnayDB.conn.commit()
+            print("Insert Data Succesfully")
+        except Exception as e:
+            print(e)
