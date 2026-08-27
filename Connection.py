6e79e71510a3e03c7207fc1490458e80d12a40b8
aksahythigale7101 <akshaythigale7101@gmail.com>
2026-08-27 14:59:20 +0530

    New emoplyee database added

--- Connection.py
+++ Connection.py
@@ -8,15 +8,16 @@ class ConnectionDatabase:
     def __init__(self, db_name):
         try:
             self.conn = sqlite3.connect(db_name)
-            print("Database connected successfully.")
+            #print("Database connected successfully.")
         except sqlite3.Error as e:
             self.conn = None
-            print(f"Database connection failed: {e}")
+            #print(f"Database connection failed: {e}")
 
 
 # Create the single instance HERE so other files can import it
 _Db = ConnectionDatabase("Collage.db")
 
+_Db1 =ConnectionDatabase("Compnay.db")
                
 
 
