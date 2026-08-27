cfe10e15fda403db1e4d05722a8c53824eeee9de
aksahythigale7101 <akshaythigale7101@gmail.com>
2026-08-27 11:45:41 +0530

    Clean up commented code and call ReadData

--- SqlLite.py
+++ SqlLite.py
@@ -1,4 +1,5 @@
-#   python -c "import sqlite3; print(sqlite3.sqlite_version)"
+#   python -c "import sqlite3; 
+#   print(sqlite3.sqlite_version)" // showing on console
 #this commond is used to check the version of sqlite3 installed in your python environment.
 
 import Connection
@@ -8,13 +9,10 @@ import Connection
 
 from CRUD import CRUD_Operation
 
-CRUD_Operation.InsertData('jow', 22, 'Jo.j@example.com', 'Computer Science');
-#CRUD_Operation.UpdateData("Pooja Kharde",2)
-#CRUD_Operation.DeleteData(10)
-CRUD_Operation.ReadData()
 
 
 
+#CRUD_Operation.InsertData('jow', 22, 'Jo.j@example.com', 'Computer Science');
 # Is.InsertData ('Rahul Sharma', 21, 'rahul@example.com', 'Computer Science');
 
 # Is.InsertData('Priya Deshmukh', 22, 'priya.d@example.com', 'Data Science');
@@ -22,3 +20,7 @@ CRUD_Operation.ReadData()
 # Is.InsertData('Sneha Kulkarni', 23, 'sneha.k@example.com', 'Information Technology');
 # Is.InsertData('Rohan Patil', 21, 'rohan.patil@example.com', 'Electronics');
 # Is.InsertData('Ananya Joshi', 22, 'ananya.j@example.com', 'Computer Science');
+
+#CRUD_Operation.UpdateData("Pooja Kharde",2)
+#CRUD_Operation.DeleteData(10)
+CRUD_Operation.ReadData()
