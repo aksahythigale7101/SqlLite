8bab4381e091e4c9548f3dccfbecf75eba0ae1fb
aksahythigale7101 <akshaythigale7101@gmail.com>
2026-08-28 18:37:28 +0530

    add new Employee table colum and add call function of join and clause

--- SqlLite.py
+++ SqlLite.py
@@ -5,8 +5,9 @@
 import Connection
 
 import Create_Table
-
+from CRUD import CRUD_Operation  #option 2 -----import CRUD
 """
+
 from CRUD import CRUD_Operation  #option 2 -----import CRUD
 
 #CRUD_Operation.InsertData('jow', 22, 'Jo.j@example.com', 'Computer Science');
@@ -20,9 +21,9 @@ from CRUD import CRUD_Operation  #option 2 -----import CRUD
 
 #CRUD_Operation.UpdateData("Pooja Kharde",2)
 #CRUD_Operation.DeleteData(10)
-CRUD_Operation.ReadData()
-"""
 
+"""
+# CRUD_Operation.ReadData()
 
 
 from Keys import keys
@@ -32,7 +33,7 @@ Iquery=(1,"IT")
 Iquery=(2,"HR")
 Iquery=(3,"Finance")
 Iquery=(4,"Sales")
-
+Iquery=(5,"Marketing")
 if keys.InsertDepat(Iquery):
    print(f"Data Insert Succesfully {Iquery[1]}")
 else:
@@ -40,19 +41,33 @@ else:
 '''
 
 
-# keys.Drop_Table("Employee")
+#keys.Drop_Table("Employee")
 
 keys.ReadData("DEPARTMENT") # Department Table is the Parent because its DepartmentId is referenced.
 
-'''
+''' #old table 
 keys.InsertEmp(101, "Akshay", 1)
-keys.InsertEmp(102, "Rahul", 2)
-keys.InsertEmp(103, "Amit", 1)#Duplicate accpect
-keys.InsertEmp(104, "Sneha", 3)
-keys.InsertEmp(105, "Abhay", 4)
-keys.InsertEmp(106, "Ranjeet",  None)# null is accpect until when we cerate table coulmn set as not null
 '''
 #keys.InsertEmp(106, "Harsh", 99) # no added becuase is not present DeptID 9 in Deparetment table DepetID  cause of forgien key
+
+'''
+keys.InsertEmp(101, 'Akshay', 30, 55000, 'Pune', 1)
+keys.InsertEmp(102, 'Rahul', 28, 60000, 'Mumbai', 1)#Duplicate accpect allow
+keys.InsertEmp(103, 'Amit', 35, 45000, 'Pune', 2)
+keys.InsertEmp(104, 'Sneha', 27, 50000, 'Mumbai', 2)#Duplicate accpect allow
+keys.InsertEmp(105, 'Vijay', 32, 75000, 'Pune', 3)
+keys.InsertEmp(106, 'Priya', 29, 65000, 'Delhi', 3)
+keys.InsertEmp(107, 'Rohit', 26, 40000, 'Pune', 4)
+keys.InsertEmp(108, 'Neha', 31, 70000, 'Mumbai', 5)
+keys.InsertEmp(109, 'Kiran', 34, 80000, 'Delhi', 5)
+keys.InsertEmp(110, 'Pooja', 25, 35000, 'Pune', 4)
+keys.InsertEmp(111, "Ranjeet",25,32500,'Pune', None)# null is accpect until when we cerate table coulmn set as not null
+'''
+
+
+
+
+
 
 
 # keys.delete_row("DEPARTMENT","DepartmentId",1)# not delete beacuse the Id 1 refreance presetn in employee table
@@ -77,8 +92,17 @@ keys.ReadData("EMPLOYEE") #Employee Table is the Child because it contains the F
 
 #------------------------------------------------------------------------
 #"-------------------Join Output-------------------------"
+'''
 from Join import _join 
 
-# _join.innerJoin()
-# _join.LeftJoin()
+_join.innerJoin()
+_join.LeftJoin()
 _join.RightJoin()
+'''
+
+#------------------------------------Group with having----------------------
+
+from Claues import claues
+
+claues.Groupby()
+claues.where()
