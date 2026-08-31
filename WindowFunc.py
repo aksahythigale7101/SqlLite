abc82c6474cbaf64313b62fc4e1b289150dab7f4
aksahythigale7101 <akshaythigale7101@gmail.com>
2026-08-31 20:56:24 +0530

    Query2 in cooment on row_Number()

--- WindowFunc.py
+++ WindowFunc.py
@@ -4,8 +4,6 @@ from Connection import _Db1 as CompanyDB
 
 cursor_comp = CompanyDB.conn.cursor()
 
-import sqlite3
-
 
 class WinFunction:
     def NoramlAggregate():
@@ -25,22 +23,40 @@ class WinFunction:
 
     def ROW_NUMBER():
         query1 = (
-            "SELECT *,"
+            "SELECT *, "
             "  ROW_NUMBER()"
             " OVER (PARTITION  BY DepartmentId) as DeptTotal "
             " FROM EMPLOYEE"
         )
+        '''
         query2 = (
-        "SELECT * FROM ("
+        "SELECT * FROM ("#EMPLOYEE टेबलमधले सगळे रेकॉर्ड्स घेतले जातात.
         "  SELECT *, "
-        "  ROW_NUMBER() OVER (PARTITION BY DepartmentId ORDER BY City DESC) AS rn "
+        "  ROW_NUMBER() "#ROW_NUMBER() म्हणजे — प्रत्येक गटात प्रत्येक रेकॉर्डला एक क्रमांक (1, 2, 3...) दिला जातो.
+                         # सगळ्यात जास्त पगार असलेल्याला 1 नंबर मिळतो, त्याखालच्याला 2, वगैरे.
+        "OVER (PARTITION BY DepartmentId " #PARTITION BY DepartmentId म्हणजे — सगळ्या employees ना त्यांच्या 
+                                                                     # DepartmentId नुसार वेगवेगळ्या गटांत (groups) विभागायचं.
+        
+        "ORDER BY salary DESC) "#ORDER BY salary DESC म्हणजे — प्रत्येक गटाच्या आत employees ना पगारानुसार #(जास्त ते कमी) क्रमवारी लावायची. 
+                                                                                        
+         "AS rn "   #हा क्रमांक rn नावाच्या नवीन कॉलममध्ये साठवला जातो.
         "  FROM EMPLOYEE"
-        ") t "
-        "WHERE rn = 1"
+        ") t " #इथे t हे फक्त एक alias (टोपणनाव) आहे
+        "WHERE rn = 1" #वरच्या रिझल्टमधून फक्त तेच रेकॉर्ड्स निवडले जातात ज्यांचा rn = 1 आहे.
     )
+    '''
+        query2 = (
+            "SELECT * FROM("
+            "SELECT *, "
+            "  ROW_NUMBER()"
+            " OVER (PARTITION  BY DepartmentId ORDER BY SALARY DESC) as rn "
+            " FROM EMPLOYEE"
+            ")t "
+            "WHERE rn = 1"
+        )
 
         # print(sqlite3.sqlite_version)
-        WinFunction.ReadData(query2, "", "Row_Number")
+        WinFunction.ReadData(query1, "", "Row_Number")
 
     @staticmethod
     def ReadData(Query, parms, comment):
