011c40e32f563374686e48341f430f960fe918b2
aksahythigale7101 <akshaythigale7101@gmail.com>
2026-09-01 18:51:00 +0530

    Junction means database connection and crete engine

---
+++ SqlAlchmy/Junction.py
@@ -0,0 +1,20 @@
+﻿from sqlalchemy import create_engine
+
+
+class ORMConnections:
+    def __init__(self, db_name):
+        try:
+            self.engine = create_engine(db_name)
+            with self.engine.connect() as conn:  # Actual connection करण्यासाठी:
+                print()
+                #print("Database connected")
+        except Exception as e:
+            self.engine = None
+            print(f"Database connection failed: {e}")
+            # sqlite3 = Python ची SQLite-specific library.
+            # SQLAlchemy = अनेक databases साठी abstraction/ORM toolkit.
+
+
+ORMDB = ORMConnections(
+    "sqlite:///SqlAlchmy/School.db"
+)  # आपण SQLite database वापरत आहोत.& /// → हा relative path आहे.(current working directory)
