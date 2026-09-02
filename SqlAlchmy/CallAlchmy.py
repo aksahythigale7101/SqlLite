ff98fe7be0d1d5159609bcc4f547166a865f2cb1
aksahythigale7101 <akshaythigale7101@gmail.com>
2026-09-02 18:33:35 +0530

    Call functions

--- SqlAlchmy/CallAlchmy.py
+++ SqlAlchmy/CallAlchmy.py
@@ -1,27 +1,28 @@
 
-from Junction import ORMDB as StudentDB
-from ORM_Table import student as StudentTable
-from ORM_CRUD import CRUDOpertions as StudentCRUD
 import json
-
-# Insert Data---------------------------
-
+from pathlib import Path
+from ORM_CRUD import CRUDOpertions as StudentCRUD
+from ORM_Filter import FilterTable as filter
 
 # StudentCRUD.DropTable()  # Drop the table if it exists
 
 
-# with open(r"E:\GitProject\SqlLite\SqlAlchmy\StudInfo.json", "r") as f:
-#     employees = json.load(f)
+existing_count = StudentCRUD.TableRowsCount()
 
+if existing_count == 0:
+    with open(Path(__file__).resolve().parent / "StudInfo.json", "r") as f:
+        employees = json.load(f)
+    for i, emp in enumerate(employees):
+        StudentCRUD.InsertTable(
+            _id=i + 1,
+            _name=emp["name"],
+            _age=emp["age"],
+            _gender=emp["gender"],
+            _email=emp["email"],
+            _DeptID=emp["DeptID"],
+        )
 
-# for i, emp in enumerate(employees):
-#     StudentCRUD.InsertTable(
-#         _id=i+1,
-#         _name=emp["name"],
-#         _age=emp["age"],
-#         _gender=emp["gender"],
-#         _email=emp["email"],
-#         _DeptID=emp["DeptID"]
-#         )
-
+# StudentCRUD.UpdateTable(7, 50);
+#StudentCRUD.DeleteRow(13)
 StudentCRUD.DisplayData()  # Display the data in the table
+filter.WhereClause()
