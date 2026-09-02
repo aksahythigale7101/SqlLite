d34a890200f6b45dff35cda37f367eaf5dc89201
aksahythigale7101 <akshaythigale7101@gmail.com>
2026-09-02 18:35:27 +0530

    Crude Oeprtion Complete

--- SqlAlchmy/ORM_CRUD.py
+++ SqlAlchmy/ORM_CRUD.py
@@ -1,14 +1,21 @@
-from ORM_Table import student as StudentTable
+﻿from turtle import update
 from Junction import ORMDB as StudentDB
+from ORM_Table import student as StudentTable
 from sqlalchemy.orm import Session
 
-from sqlalchemy import text
+from sqlalchemy import select, func, text
 from tabulate import tabulate
+from sqlalchemy import update as _update
+from sqlalchemy import delete as _delete
+
+
 
 class CRUDOpertions:
     def InsertTable(_id, _name, _age, _gender, _email, _DeptID):
         try:
-            with Session(StudentDB.engine) as session:
+            with Session(
+                StudentDB.engine
+            ) as session:  # WITH-याचा फायदा म्हणजे काम झाल्यावर session व्यवस्थित close होतो.
                 new_student = StudentTable(
                     id=_id,
                     name=_name,
@@ -17,11 +24,78 @@ class CRUDOpertions:
                     email=_email,
                     DeptID=_DeptID,
                 )
-                session.add(new_student)
-                session.commit()
+                session.add(new_student)  # "हा object save करण्यासाठी तयार ठेव."
+                session.commit()  # "आता transaction database मध्ये permanently save कर."
             print("Data inserted successfully.")
         except Exception as e:
             print(f"Error inserting data: {e}")
+            print(type(e).__name__)  # Print the type of the exception
+
+    def TableRowsCount():
+        with Session(StudentDB.engine) as session:
+            count = session.query(func.count(StudentTable.id)).scalar()
+            return count
+        
+    def UpdateTable(_id, _age):
+        try:
+            with Session(StudentDB.engine) as session:
+                # using get through updatae #by using get फक्त primary key वरून एक record हवा (उदा. ID)
+                # stud=session.get(StudentTable,_id)
+                # if stud:
+                #     stud.age=_age
+                #     session.commit()
+                # else:
+                #      print(f"Session {_id} is not found.")
+
+                # using query through filter and change data
+                # stud1=session.query(StudentTable).filter(StudentTable.id==_id).first()
+                # stud1.age=_age;
+                # session.commit()
+
+                # using scalars
+                # scal=select(StudentTable).where(StudentTable.id==_id)
+                # stud2=session.scalars(scal).first()
+                # stud2.age=_age;
+                # session.commit()
+
+                # using normal method and this is not sql alchmy method
+                stmt = (
+                    _update(StudentTable).where(StudentTable.id == _id).values(age=_age)
+                )
+                stud3 = session.execute(stmt)
+                session.commit()
+        except Exception as e:
+            print(f"Error updating data: {e}")
+            print(type(e).__name__)  # Print the type of the exception
+
+    def DeleteRow(_id):
+        try:
+            with Session(StudentDB.engine) as session:
+                # stud = session.get(StudentTable, _id)
+                # if stud:
+                #     session.delete(stud)
+                #     session.commit()
+                #     print(f"Session Deleted : {_id} Successfully.")
+                # else:
+                #     print(f"Session {_id} is not found.")
+
+                # scal=select(StudentTable).where(StudentTable.id==_id)
+                # stud2=session.scalars(scal).first()
+                # session.delete(stud2)
+                # session.commit()
+
+                stmt = (
+                    _delete(StudentTable).where(StudentTable.id == _id)
+                )
+                stud3 = session.execute(stmt)
+                session.commit()
+
+
+
+                session.commit()
+
+        except Exception as e:
+            print(f"Error updating data: {e}")
             print(type(e).__name__)  # Print the type of the exception
 
     def DropTable():
@@ -38,7 +112,10 @@ class CRUDOpertions:
         try:
             with Session(StudentDB.engine) as session:
                 Stud = session.query(StudentTable).all()
-                rows = [[s.id, s.name, s.age, s.gender, s.email, s.DeptID] for s in Stud]
+
+                rows = [
+                    [s.id, s.name, s.age, s.gender, s.email, s.DeptID] for s in Stud
+                ]
                 headers = ["ID", "Name", "Age", "Gender", "Email", "DeptID"]
                 print(tabulate(rows, headers=headers, tablefmt="grid"))
                 # for student in rows:
@@ -49,3 +126,23 @@ class CRUDOpertions:
         except Exception as e:
             print(f"Error displaying data: {e}")
             print(type(e).__name__)  # Print the type of the exception
+
+            # -------------------------------------------------------------------------------------------------------------------------------------------------
+            # region this recore display method
+            """
+            stnt = select(StudentTable).where(StudentTable.id == 5)
+                r = session.scalars(stnt)#वापरल्यावर तो automatically tuple मधून पहिला element काढून देतो
+                # print(st[2].email)
+                # ro = r.first()#.first() वापरून तुम्हाला त्यातला फक्त पहिला result मिळतो.
+                # ro = r.one()#.one() तेव्हाच वापरा जेव्हा तुम्हाला खात्री आहे की query मधून फक्त एकच result येईल — उदा. id किंवा email सारख्या unique column वर filter करताना:
+               #print(ro.name)
+                print("-------------")
+
+                stnt1 = select(StudentTable.name, StudentTable.age)
+                result = session.execute(stnt1)#.execute() वापरल्यावर प्रत्येक row Row object (tuple सारखं) स्वरूपात मिळतो
+                # for row in result:
+                #     print(row)
+
+                print("-------------")
+            """
+            # endregion
