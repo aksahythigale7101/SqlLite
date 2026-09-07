76ffa1eb659975aec95c024a74ed32c43b71f9d3
aksahythigale7101 <akshaythigale7101@gmail.com>
2026-09-07 17:13:17 +0530

    Remove unused Imports

--- SqlAlchmy/CallAlchmy.py
+++ SqlAlchmy/CallAlchmy.py
@@ -1,12 +1,8 @@
-import json
-from pathlib import Path
 
-from sqlalchemy import orm
-from ORM_CRUD import CRUDOpertions as StudentCRUD
-from ORM_Filter import FilterTable 
-from ORM_Key import Link
-from ORM_Join import joins
-from SqlAlchmy import ORM_Filter
+
+
+
+
 from funcAssgine import display_menu,get_choice,handle_menu,seed_students_if_empty,clear_console
 
 
